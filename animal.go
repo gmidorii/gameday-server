8c22604e2de0e4846c337ab75fe62e67db34031a
soichiro-taga <soichiro-taga@navitime.co.jp>
2017-08-28 18:58:28 +0900

    Add POST animal

--- animal.go
+++ animal.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+// Animals is multiple Animal struct
+type Animals struct {
+	Animals []Animal
+}
 
 // Animal is `t_animal` table mapping struct
 type Animal struct {
@@ -25,6 +30,75 @@ func animalHandler(w http.ResponseWriter, r *http.Request) {
 
 func animalGET(w http.ResponseWriter, r *http.Request) {
 	fID := r.URL.Query().Get("id")
+
+	animals, err := selectAnimals(fID)
+	if err != nil {
+		failed(err, w)
+		return
+	}
+	tmpl, err := template.ParseFiles("./template/animal.html")
+	if err != nil {
+		failed(err, w)
+		return
+	}
+	tmpl.Execute(w, animals)
+}
+
+func animalPOST(w http.ResponseWriter, r *http.Request) {
+	fName := r.FormValue("name")
+	if fName == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("required `name` parameter"))
+		return
+	}
+
+	if err := insertAnimals(fName); err != nil {
+		failed(err, w)
+		return
+	}
+
+	animals, err := selectAnimals("")
+	if err != nil {
+		failed(err, w)
+		return
+	}
+	tmpl, err := template.ParseFiles("./template/animal.html")
+	if err != nil {
+		failed(err, w)
+		return
+	}
+	tmpl.Execute(w, animals)
+}
+
+func insertAnimals(fName string) error {
+	db, err := sql.Open("mysql",
+		fmt.Sprintf("%s:%s@tcp(%s:3306)/animal", cfg.Animal.User, cfg.Animal.Password, cfg.Animal.Host))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	insertAnimal, err := db.Prepare("INSERT INTO t_animal (name) values(?)")
+	if err != nil {
+		return err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Stmt(insertAnimal).Exec(fName)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+
+	return nil
+}
+
+func selectAnimals(fID string) (Animals, error) {
 	db, err := sql.Open("mysql",
 		fmt.Sprintf("%s:%s@tcp(%s:3306)/animal", cfg.Animal.User, cfg.Animal.Password, cfg.Animal.Host))
 	if err != nil {
@@ -38,55 +112,18 @@ func animalGET(w http.ResponseWriter, r *http.Request) {
 	}
 	rows, err := db.Query(sql)
 	if err != nil {
-		failed(err, w)
-		return
+		return Animals{}, err
 	}
 	defer rows.Close()
 
-	var animals []Animal
+	var animals Animals
 	for rows.Next() {
 		var animal Animal
 		rows.Scan(&animal.ID, &animal.Name)
 
-		animals = append(animals, animal)
+		animals.Animals = append(animals.Animals, animal)
 	}
-
-	jsonAnimal, err := json.Marshal(animals)
-	if err != nil {
-		failed(err, w)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonAnimal)
-}
-
-func animalPOST(w http.ResponseWriter, r *http.Request) {
-	fName := r.FormValue("name")
-	if fName == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("required `name` parameter"))
-		return
-	}
-	db, err := sql.Open("mysql",
-		fmt.Sprintf("%s:%s@tcp(%s:3306)/animal", cfg.Animal.User, cfg.Animal.Password, cfg.Animal.Host))
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	insertAnimal, err := db.Prepare("INSERT INTO name values(?)")
-	if err != nil {
-		failed(err, w)
-		return
-	}
-
-	tx, err := db.Begin()
-	if err != nil {
-		failed(err, w)
-		return
-	}
-
-	result, err := tx.Stmt(insertAnimal).Exec(fName)
+	return animals, nil
 }
 
 func failed(err error, w http.ResponseWriter) {
